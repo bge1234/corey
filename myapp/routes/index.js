8daf87d9388b5a027d9f2ebe58a634c087af918f
Rachel Parri <erachelparri@gmail.com>
2016-01-08 10:46:07 -0700

    printing out all objects

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')({
+    client: 'pg',
+    connection: 'postgres://localhost/restaurants'
+});
+
+function getZagat() {
+  return knex('zagat')
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  getZagat().select().then(function(result){
+    res.render('index', {zagat: result})
+  })
 });
 
 module.exports = router;
